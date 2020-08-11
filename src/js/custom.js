f27dad81805c1bcb3f072b7d4568364a3d0ccf06
Ben Elwood <ben.elwoods@gmail.com>
2020-08-12 09:53:02 +1200

    firt commit

---
+++ src/js/custom.js
@@ -0,0 +1,22 @@
+// Pseudo code
+
+// 1. Create table
+// 2. add fields in table
+
+let app = new Vue ({
+  el: '#app',
+  data: {
+    listData: {
+
+    }
+  },
+  // created: {
+  //
+  // },
+  // mounted: {
+  //
+  // },
+  // methods: {
+  //
+  // }
+})
